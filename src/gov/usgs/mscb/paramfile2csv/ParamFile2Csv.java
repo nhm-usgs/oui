5badd5727c05a6374d3ec3a2b1886a6376b2aacf
Markstrom, Steven L <markstro@usgs.gov>
2017-06-19 10:52:04 -0600

    unknown

--- src/gov/usgs/mscb/paramfile2csv/ParamFile2Csv.java
+++ src/gov/usgs/mscb/paramfile2csv/ParamFile2Csv.java
@@ -4,7 +4,7 @@
  */
 package gov.usgs.mscb.paramfile2csv;
 
-import csvutils.CsvWriter;
+import gov.usgs.mscb.csvutils.CsvWriter;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
